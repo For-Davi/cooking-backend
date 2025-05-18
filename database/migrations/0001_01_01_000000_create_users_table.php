306e09b98bdd154a393f33a5d20312a08928c6e6
Carlos Davi <carlosdavi004@gmail.com>
2025-05-18 14:44:55 -0300

    Build: Instalação do laravel sail

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -17,6 +14,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'common'])->default('common');
+            $table->enum('active', [1, 0])->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
