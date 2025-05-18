3922f0d12d36c0083283c10454a835faeb60b72d
Carlos Davi <carlosdavi004@gmail.com>
2025-05-18 17:09:13 -0300

    Feat: Comentado linhas de codigo

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
-Route::post('/reset', [UserController::class, 'reset']);
-Route::post('/verify', [UserController::class, 'verify']);
-Route::post('/newPassword', [UserController::class, 'resetPassword']);
+// Route::post('/reset', [UserController::class, 'reset']);
+// Route::post('/verify', [UserController::class, 'verify']);
+// Route::post('/newPassword', [UserController::class, 'resetPassword']);
