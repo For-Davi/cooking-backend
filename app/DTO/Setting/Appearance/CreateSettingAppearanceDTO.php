6a6e8861779d05294de57117b9e15e1f4c0ad10e
Luiz Carlos <luizcarlosdev000@gmail.com>
2025-08-18 15:20:36 -0300

    feat/implementado endpoint para alteração da aparência de title page

--- app/DTO/Setting/Appearance/CreateSettingAppearanceDTO.php
+++ app/DTO/Setting/Appearance/CreateSettingAppearanceDTO.php
@@ -6,12 +6,14 @@ class CreateSettingAppearanceDTO
 {
     public function __construct(
         public readonly int $enterprise_id,
+        public readonly int $title_page_color_default,
         public readonly int $navbar_color_default,
         public readonly int $navbar_icon_color_default,
         public readonly int $side_menu_color_default_not_selected_item,
         public readonly int $side_menu_color_default_selected_item,
         public readonly int $side_menu_color_default_not_selected_icon,
         public readonly int $side_menu_color_default_selected_icon,
+        public readonly ?string $title_page_color_code,
         public readonly ?string $navbar_color_code,
         public readonly ?string $navbar_icon_color_code,
         public readonly ?string $side_menu_color_code_not_selected_item,
@@ -24,12 +26,14 @@ class CreateSettingAppearanceDTO
     {
         return new self(
             enterprise_id: $data['enterpriseID'],
+            title_page_color_default: 1,
             navbar_color_default: 1,
             navbar_icon_color_default: 1,
             side_menu_color_default_not_selected_item: 1,
             side_menu_color_default_selected_item: 1,
             side_menu_color_default_not_selected_icon: 1,
             side_menu_color_default_selected_icon: 1,
+            title_page_color_code: null,
             navbar_color_code: null,
             navbar_icon_color_code: null,
             side_menu_color_code_not_selected_item: null,
@@ -43,12 +47,14 @@ class CreateSettingAppearanceDTO
     {
         return [
             'enterprise_id' => $this->enterprise_id,
+            'title_page_color_default' => $this->title_page_color_default,
             'navbar_color_default' => $this->navbar_color_default,
             'navbar_icon_color_default' => $this->navbar_icon_color_default,
             'side_menu_color_default_not_selected_item' => $this->side_menu_color_default_not_selected_item,
             'side_menu_color_default_selected_item' => $this->side_menu_color_default_selected_item,
             'side_menu_color_default_not_selected_icon' => $this->side_menu_color_default_not_selected_icon,
             'side_menu_color_default_selected_icon' => $this->side_menu_color_default_selected_icon,
+            'title_page_color_code' => $this->title_page_color_code,
             'navbar_color_code' => $this->navbar_color_code,
             'navbar_icon_color_code' => $this->navbar_icon_color_code,
             'side_menu_color_code_not_selected_item' => $this->side_menu_color_code_not_selected_item,
--- app/Http/Requests/Setting/Appearance/UpdateSettingAppearanceRequest.php
+++ app/Http/Requests/Setting/Appearance/UpdateSettingAppearanceRequest.php
@@ -14,12 +14,14 @@ class UpdateSettingAppearanceRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'titlePageColorDefault' => 'required|in:0,1',
             'navbarColorDefault' => 'required|in:0,1',
             'navbarIconColorDefault' => 'required|in:0,1',
             'sideMenuColorDefaultNotSelectedItem' => 'required|in:0,1',
             'sideMenuColorDefaultSelectedItem' => 'required|in:0,1',
             'sideMenuColorDefaultNotSelectedIcon' => 'required|in:0,1',
             'sideMenuColorDefaultSelectedIcon' => 'required|in:0,1',
+            'titlePageColorCode' => 'required_if:titlePageColorDefault,0|nullable|string|max:7|min:7',
             'navbarColorCode' => 'required_if:navbarColorDefault,0|nullable|string|max:7|min:7',
             'navbarIconColorCode' => 'required_if:navbarIconColorDefault,0|nullable|string|max:7|min:7',
             'sideMenuColorCodeNotSelectedItem' => 'required_if:sideMenuColorDefaultNotSelectedItem,0|nullable|string|max:7|min:7',
@@ -32,47 +34,54 @@ class UpdateSettingAppearanceRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'navbarColorDefault.required' => 'Deve ser requerido o campo do toggle',
-            'navbarColorDefault.in' => 'O valor do toggle deve ser 0 ou 1',
+            'titlePageColorDefault.required' => 'Deve ser requerido o campo do toggle de título',
+            'titlePageColorDefault.in' => 'O valor do toggle de título deve ser 0 ou 1',
 
-            'navbarIconColorDefault.required' => 'Deve ser requerido o campo do toggle',
-            'navbarIconColorDefault.in' => 'O valor do toggle deve ser 0 ou 1',
+            'navbarColorDefault.required' => 'Deve ser requerido o campo do toggle da navbar',
+            'navbarColorDefault.in' => 'O valor do toggle da navbar deve ser 0 ou 1',
 
-            'sideMenuColorDefaultNotSelectedItem.required' => 'Deve ser requerido o campo do toggle',
-            'sideMenuColorDefaultNotSelectedItem.in' => 'O valor do toggle deve ser 0 ou 1',
+            'navbarIconColorDefault.required' => 'Deve ser requerido o campo do toggle dos ícones da navbar',
+            'navbarIconColorDefault.in' => 'O valor do toggle dos ícones da navbar deve ser 0 ou 1',
 
-            'sideMenuColorDefaultSelectedItem.required' => 'Deve ser requerido o campo do toggle',
-            'sideMenuColorDefaultSelectedItem.in' => 'O valor do toggle deve ser 0 ou 1',
+            'sideMenuColorDefaultNotSelectedItem.required' => 'Deve ser requerido o campo do toggle de itens não selecionados',
+            'sideMenuColorDefaultNotSelectedItem.in' => 'O valor do toggle de itens não selecionados deve ser 0 ou 1',
 
-            'sideMenuColorDefaultNotSelectedIcon.required' => 'Deve ser requerido o campo do toggle',
-            'sideMenuColorDefaultNotSelectedIcon.in' => 'O valor do toggle deve ser 0 ou 1',
+            'sideMenuColorDefaultSelectedItem.required' => 'Deve ser requerido o campo do toggle de itens selecionados',
+            'sideMenuColorDefaultSelectedItem.in' => 'O valor do toggle de itens selecionados deve ser 0 ou 1',
 
-            'sideMenuColorCodeSelectedIcon.required' => 'Deve ser requerido o campo do toggle',
-            'sideMenuColorCodeSelectedIcon.in' => 'O valor do toggle deve ser 0 ou 1',
+            'sideMenuColorDefaultNotSelectedIcon.required' => 'Deve ser requerido o campo do toggle de ícones não selecionados',
+            'sideMenuColorDefaultNotSelectedIcon.in' => 'O valor do toggle de ícones não selecionados deve ser 0 ou 1',
 
-            'navbarColorCode.string' => 'O código da cor deve ser uma string',
-            'navbarColorCode.min' => 'O código da cor deve conter 7 caracteres',
-            'navbarColorCode.max' => 'O código da cor deve conter 7 caracteres',
+            'sideMenuColorCodeSelectedIcon.required' => 'Deve ser requerido o campo do toggle de ícones selecionados',
+            'sideMenuColorCodeSelectedIcon.in' => 'O valor do toggle de ícones selecionados deve ser 0 ou 1',
 
-            'navbarIconColorCode.string' => 'O código da cor deve ser uma string',
-            'navbarIconColorCode.min' => 'O código da cor deve conter 7 caracteres',
-            'navbarIconColorCode.max' => 'O código da cor deve conter 7 caracteres',
+            'titlePageColorCode.string' => 'O código da cor de título deve ser uma string',
+            'titlePageColorCode.min' => 'O código da cor de título deve conter 7 caracteres',
+            'titlePageColorCode.max' => 'O código da cor de título deve conter 7 caracteres',
 
-            'sideMenuColorCodeNotSelectedItem.string' => 'O código da cor deve ser uma string',
-            'sideMenuColorCodeNotSelectedItem.min' => 'O código da cor deve conter 7 caracteres',
-            'sideMenuColorCodeNotSelectedItem.max' => 'O código da cor deve conter 7 caracteres',
+            'navbarColorCode.string' => 'O código da cor do navbar deve ser uma string',
+            'navbarColorCode.min' => 'O código da cor do navbar deve conter 7 caracteres',
+            'navbarColorCode.max' => 'O código da cor do navbar deve conter 7 caracteres',
 
-            'sideMenuColorCodeSelectedItem.string' => 'O código da cor deve ser uma string',
-            'sideMenuColorCodeSelectedItem.min' => 'O código da cor deve conter 7 caracteres',
-            'sideMenuColorCodeSelectedItem.max' => 'O código da cor deve conter 7 caracteres',
+            'navbarIconColorCode.string' => 'O código da cor de ícones do navbar deve ser uma string',
+            'navbarIconColorCode.min' => 'O código da cor de ícones do navbar deve conter 7 caracteres',
+            'navbarIconColorCode.max' => 'O código da cor de ícones do navbar deve conter 7 caracteres',
 
-            'sideMenuColorCodeNotSelectedIcon.string' => 'O código da cor deve ser uma string',
-            'sideMenuColorCodeNotSelectedIcon.min' => 'O código da cor deve conter 7 caracteres',
-            'sideMenuColorCodeNotSelectedIcon.max' => 'O código da cor deve conter 7 caracteres',
+            'sideMenuColorCodeNotSelectedItem.string' => 'O código da cor de itens não selecionados deve ser uma string',
+            'sideMenuColorCodeNotSelectedItem.min' => 'O código da cor de itens não selecionados deve conter 7 caracteres',
+            'sideMenuColorCodeNotSelectedItem.max' => 'O código da cor de itens não selecionados deve conter 7 caracteres',
 
-            'sideMenuColorCodeSelectedIcon.string' => 'O código da cor deve ser uma string',
-            'sideMenuColorCodeSelectedIcon.min' => 'O código da cor deve conter 7 caracteres',
-            'sideMenuColorCodeSelectedIcon.max' => 'O código da cor deve conter 7 caracteres',
+            'sideMenuColorCodeSelectedItem.string' => 'O código da cor de itens selecionados deve ser uma string',
+            'sideMenuColorCodeSelectedItem.min' => 'O código da cor de itens selecionados deve conter 7 caracteres',
+            'sideMenuColorCodeSelectedItem.max' => 'O código da cor de itens selecionados deve conter 7 caracteres',
+
+            'sideMenuColorCodeNotSelectedIcon.string' => 'O código da cor de ícones não selecionados deve ser uma string',
+            'sideMenuColorCodeNotSelectedIcon.min' => 'O código da cor de ícones não selecionados deve conter 7 caracteres',
+            'sideMenuColorCodeNotSelectedIcon.max' => 'O código da cor de ícones não selecionados deve conter 7 caracteres',
+
+            'sideMenuColorCodeSelectedIcon.string' => 'O código da cor de ícones selecionados deve ser uma string',
+            'sideMenuColorCodeSelectedIcon.min' => 'O código da cor de ícones selecionados deve conter 7 caracteres',
+            'sideMenuColorCodeSelectedIcon.max' => 'O código da cor de ícones selecionados deve conter 7 caracteres',
 
         ];
     }
